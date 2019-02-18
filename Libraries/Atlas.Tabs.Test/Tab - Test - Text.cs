b5c7f16a4dbc6e57b069226e343b625bbdc8197f
Gary Hertel <garyhertel@gmail.com>
2019-02-17 21:42:04 -0800

    Added "Short Text" to tests

--- Libraries/Atlas.Tabs.Test/Tab - Test - Text.cs
+++ Libraries/Atlas.Tabs.Test/Tab - Test - Text.cs
@@ -13,12 +13,14 @@ namespace Atlas.Tabs.Test
 		{
 			public override void Load()
 			{
+				string shortText = GetString(1000);
 				string longText = GetString(10000);
 				string reallyLongText = GetString(100000);
 
 				tabModel.Items = new ItemCollection<ListItem>()
 				{
 					new ListItem("Sample Text", "This is some sample text\n\n1\n2\n3"),
+					new ListItem("Short Text - [" + shortText.Length.ToString() + "]", shortText),
 					new ListItem("Medium Text - [" + longText.Length.ToString() + "]", longText),
 					new ListItem("Long Text - [" + reallyLongText.Length.ToString() + "]", reallyLongText),
 				};
