5f847308637d3cc33a8239fe56ee85e22f3764cf
Gary Hertel <garyhertel@gmail.com>
2021-01-10 16:32:29 -0800

    Added HttpClient.GetHead(), and changed GetBytes() to return a response too

--- Libraries/Atlas.Network/HTTP/HttpClient.cs
+++ Libraries/Atlas.Network/HTTP/HttpClient.cs
@@ -13,14 +13,28 @@ namespace Atlas.Network
 
 		public static string GetString(Call call, string uri)
 		{
-			byte[] bytes = GetBytes(call, uri);
+			byte[] bytes = GetBytes(call, uri)?.Bytes;
 			if (bytes != null)
 				return Encoding.ASCII.GetString(bytes);
 
 			return null;
 		}
 
-		public static byte[] GetBytes(Call call, string uri)
+		public class ByteResponse
+		{
+			public WebResponse Response { get; set; }
+			public byte[] Bytes { get; set; }
+
+			public ByteResponse() {}
+
+			public ByteResponse(WebResponse response, byte[] bytes)
+			{
+				Response = response;
+				Bytes = bytes;
+			}
+		}
+
+		public static ByteResponse GetBytes(Call call, string uri)
 		{
 			using (CallTimer getCall = call.Timer("Get Uri", new Tag("Uri", uri)))
 			{
@@ -41,10 +55,10 @@ namespace Atlas.Network
 						byte[] data = memoryStream.ToArray();
 
 						dataStream.Close();
-						response.Close();
-						call.Log.Add("Downloaded Http File", new Tag("Uri", request.RequestUri), new Tag("Size", memoryStream.Length));
+						//response.Close(); // We want the Headers still (might need to copy them elsewhere if this causes problems)
+						call.Log.Add("Uri Response", new Tag("Uri", request.RequestUri), new Tag("Size", memoryStream.Length));
 
-						return data;
+						return new ByteResponse(response, data);
 					}
 					catch (WebException exception)
 					{
@@ -54,6 +68,43 @@ namespace Atlas.Network
 						{
 							string response = new StreamReader(exception.Response.GetResponseStream()).ReadToEnd();
 							getCall.Log.AddError("Exception: " + response);
+						}
+
+						if (exception.Status == WebExceptionStatus.ProtocolError)
+							break;
+					}
+				}
+				return null;
+			}
+		}
+
+		public static WebResponse GetHead(Call call, string uri)
+		{
+			using (CallTimer headCall = call.Timer("Head Uri", new Tag("Uri", uri)))
+			{
+				for (int attempt = 1; attempt <= MaxAttempts; attempt++)
+				{
+					if (attempt > 1)
+						System.Threading.Thread.Sleep(SleepMilliseconds * (int)Math.Pow(2, attempt));
+
+					HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri); // requests can't be reused between attempts
+					request.Method = "HEAD";
+					try
+					{
+						WebResponse response = request.GetResponse();
+						//response.Close();
+						call.Log.Add("Uri Response", new Tag("Uri", request.RequestUri), new Tag("Response", response));
+
+						return response;
+					}
+					catch (WebException exception)
+					{
+						headCall.Log.Add(exception);
+
+						if (exception.Response != null)
+						{
+							string response = new StreamReader(exception.Response.GetResponseStream()).ReadToEnd();
+							headCall.Log.AddError("Exception: " + response);
 						}
 
 						if (exception.Status == WebExceptionStatus.ProtocolError)
