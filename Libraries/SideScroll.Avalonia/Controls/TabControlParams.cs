c1e4c9bc7882365454403872e3269cb940949233
Gary Hertel <32446542+garyhertel@users.noreply.github.com>
2025-04-19 18:10:04 -0700

    Theming tweaks
    - Added a 1 pixel margin to default param controls
    - Changed Light Foreground color to be lighter
    - Import svg tweaks

--- Libraries/SideScroll.Avalonia/Controls/TabControlParams.cs
+++ Libraries/SideScroll.Avalonia/Controls/TabControlParams.cs
@@ -230,6 +230,7 @@ public class TabControlParams : Border, IValidationControl
 			return null;
 
 		property.Cachable = false;
+		control.Margin = new Thickness(1);
 
 		int columnIndex = property.GetCustomAttribute<ColumnIndexAttribute>()?.Index ?? 0;
 		int rowIndex = ContainerGrid.RowDefinitions.Count;
